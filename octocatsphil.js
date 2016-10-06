9091049f78d45623001819e40998f171eb31523e
PhilLutkovsky <7plutkovsky@stpatrick.org>
2016-10-06 14:11:08 -0500

    Phil Octocat Repo

---
+++ octocatsphil.js
@@ -0,0 +1,21 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+
+function Octocats(n,l) {
+
+  this.name = n;
+  this.legs = l;
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
