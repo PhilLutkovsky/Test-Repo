5a0f691e4f2292e0e8bb854ec6f657da35653994
PhilLutkovsky <7plutkovsky@stpatrick.org>
2016-10-07 12:55:42 -0500

    Finished the Octocats and successfully slapped three cats

--- octocatsphil.js
+++ octocatsphil.js
@@ -1,21 +1,21 @@
 //Write your constructor function below.
 //You should have 2 instance variables and 1 instance function included.
 
-function Octocats(n,l) {
+function Octocats(n,a) {
 
   this.name = n;
-  this.legs = l;
-  
+  this.arms = a;
 
+  this.slap = function(){
+    for(var i=0; i<this.arms; i++){
+      console.log("SLAP!")
+    }
+  };
 }
 
-
-
-
-
-
-
-
-
-
 //Write your 3 new octocat objects below here.
+var phil = new Octocat("Phil",100);
+var erik = new Octocat("Euler",.5);
+var natalie = new Octocat("Natalie",22);
+
+natalie.slap();
